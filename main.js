3462b17535513e646785c59882b8305c31c8b289
Levi <86807700+BioShot@users.noreply.github.com>
2021-10-03 11:15:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var download_button = document.getElementById("download_button")
+
+
+function download(){
+
+document.URL.location = "https://github.com/BioShot/AniPlex/archive/refs/tags/A.zip"
+
+
+}
+
+
+
+
+
+
+download_button.addEventListener('click',download)
